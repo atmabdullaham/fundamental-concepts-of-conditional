98bfdfa4b09854157db5cde94f58c79c78ccc9f3
atmabdullaham <atmabdullaham@gmail.com>
2024-11-05 23:44:25 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,29 @@
+// Variable is a Container
+/**
+ * 
+ */
+
+// NUmber
+var sunglassPrice = 80;
+console.log(sunglassPrice)
+
+// String
+var subject = "EEE";
+
+// Boolean
+var passed = true;
+
+/**
+ * Naming Convention:
+ * 1. Single Word
+ * 2. No quote
+ * 3. No gap of dash 
+ * 4. Cannot start with number
+ * 5. cannot be a keyword
+ * 6. Prefer CamelCase
+ * 
+ */
+
+var lastExamResult = 85;
+
+/
