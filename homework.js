e8cf4a8fe4f42993bbf6076fe6c176924f9da203
atmabdullaham <atmabdullaham@gmail.com>
2024-11-06 15:05:25 +0600

    Home work and instructions added

---
+++ homework.js
@@ -0,0 +1,14 @@
+// Search github programming hero "conditional task"
+/**
+ * go there and do practice for the problems from here
+ *
+ * 1. BMI Calculator
+ * 2. Grade Calculator
+ * 3. nested friends
+ * 4. ternary numbers
+ * 5. Ticket fare Calculator
+ */
+
+// "Making decisions in your code" search in google and read and practice and practice
+
+// and take notes 
