aa5b237fc27911ee829415694ff360b9bae6d95b
atmabdullaham <atmabdullaham@gmail.com>
2024-11-06 12:17:58 +0600

    start the knowing if statement

---
+++ if.js
@@ -0,0 +1,54 @@
+/**
+ * Tui jodi amake kawas ami asbo,
+ * 
+ * if you buy food for me i will come,
+ * 
+ * 
+ * if briyani price less than 300 taka i will buy
+ * 
+ * STRUCTURE
+ * 
+ *  if (condition) {
+ * 
+ * }
+ * 
+ * 
+ * if (condition) {
+ * 
+ * }
+ * 
+ * 
+ */
+
+if (3 < 10) {
+ // console.log("I have a smaller value")
+}
+
+var briyaniPrice = 500;
+
+// if (briyaniPrice < 300) {
+//  console.log("Mama give me briany")
+// }
+// if (briyaniPrice < 300) {
+//  console.log("Mama give me briany")
+// }
+
+var weight = 20;
+
+if (weight <= 20) {
+ console.log('I will carry it')
+}
+else {
+ console.log("I will rent a rickshaw")
+}
+
+let a = 2;
+if (a <= 5) {
+ console.log("a is smaller")
+}
+
+// if statement
+// the if statement execute a statement if a specified condition  is truthy
+
+// else
+//  if the condition is falsy, else statement executed.
