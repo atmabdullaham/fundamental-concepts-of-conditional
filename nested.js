8bc962346111a6e1a68fa3233ed23874aa8dae55
atmabdullaham <atmabdullaham@gmail.com>
2024-11-06 14:21:38 +0600

    Nested if-Else conditions practice

---
+++ nested.js
@@ -0,0 +1,16 @@
+const money = 0;
+if (money > 300) {
+ console.log('bro you are rich!!');
+}
+else {
+ if (money > 100) {
+  console.log('toi gorib o na rich o na')
+ }
+ else {
+  if (money > 0) {
+   console.log('dosto kola ka')
+  } else {
+   console.log('toi amar friend na')
+  }
+ }
+}
