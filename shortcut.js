a5cbcd86c7cf7c076a90c265ee84e0abe1baa5d3
atmabdullaham <atmabdullaham@gmail.com>
2024-11-06 14:56:33 +0600

    Logical Not Operator

---
+++ shortcut.js
@@ -0,0 +1,13 @@
+const isLeader = true;
+// if (isLeader) {
+//  console.log('Leader asche!')
+// } else {
+//  console.log('vitore dokte dibi na')
+// }
+
+const isPassed = false;
+if (!isPassed) {
+ console.log('toke biya dia dibo')
+} else {
+
+}
